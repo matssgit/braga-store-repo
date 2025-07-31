82abf6fad41dd8db63371d113876093bce73348f
Matheus Santana <mssdev4@gmail.com>
2025-07-31 17:58:01 -0300

    upd

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  root: resolve(__dirname, "src"),
+  build: {
+    outDir: "../dist",
+  },
 });
